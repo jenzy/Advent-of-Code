3c95f1cd8f440645018f66b998a1e05a487bd238
Jani Bevk <jani.bevk@hotmail.com>
2017-12-01 13:16:31 +0100

    day 1

---
+++ advent-of-code-2017/Day1/Day1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AdventOfCode2017
+{
+    public class Day1
+    {
+        public void Part1(string input)
+        {
+            int sum = 0;
+
+            for(int i = 0; i<input.Length; i++)
+            {
+                if (input[i] == input[(i + 1) % input.Length])
+                    sum += (int)char.GetNumericValue(input[i]);
+            }
+
+            Console.WriteLine("Result: " + sum);
+        }
+
+        public void Part2(string input)
+        {
+            int sum = 0;
+            int offset = input.Length / 2;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == input[(i + offset) % input.Length])
+                    sum += (int)char.GetNumericValue(input[i]);
+            }
+
+            Console.WriteLine("Result: " + sum);
+        }
+    }
+}
