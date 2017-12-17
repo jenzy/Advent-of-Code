cda8a5e6cd77bbe521077b7fbe3579a8f8646665
Jani Bevk <bevk.jani@gmail.com>
2017-12-17 08:50:08 +0100

    day 17

---
+++ advent-of-code-2017/Days/Day17.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2017.Days
+{
+    internal class Day17 : IDay
+    {
+        public void Part1(string input)
+        {
+            int steps = int.Parse(input);
+            var list = new LinkedList<int>();
+            list.AddFirst(0);
+            var current = list.First;
+
+            void Insert(int value)
+            {
+                for (int i = 0; i < steps; i++)
+                    current = current.Next ?? list.First;
+
+                current = list.AddAfter(current, value);
+            }
+
+            for (int i = 1; i <= 2017; i++)
+                Insert(i);
+
+            Console.WriteLine("Result: " + current.Next.Value);
+        }
+
+        public void Part2(string input)
+        {
+            int current = 0, result = 0, steps = int.Parse(input);
+
+            for (int i = 1; i <= 50_000_000; i++)
+            {
+                current = (current + steps) % i + 1;
+                if (current == 1)
+                    result = i;
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
