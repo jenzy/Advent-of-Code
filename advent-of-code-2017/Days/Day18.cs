ca8260aa336457b276d07a6dc1d713ca9dd16681
Jani Bevk <bevk.jani@gmail.com>
2017-12-18 07:57:52 +0100

    day 18 part 2

--- advent-of-code-2017/Days/Day18.cs
+++ advent-of-code-2017/Days/Day18.cs
@@ -19,39 +19,92 @@ namespace AdventOfCode2017.Days
 
         public void Part2(string input)
         {
-            Console.WriteLine("Result: " + Common(input, input.Length/2));
+            var commands = input.Split('\n').Select(x => x.Trim()).ToList();
+
+            Queue<long> q0 = new Queue<long>(), q1 = new Queue<long>();
+            var p0 = new Prog(commands, 0, q0, q1);
+            var p1 = new Prog(commands, 1, q1, q0);
+
+            int blckCount = 0;
+            while (true)
+            {
+                p0.Run();
+
+                p1.Run();
+
+
+                if (p0.Blocked && p1.Blocked)
+                {
+                    blckCount++;
+                    if (p0.BlcCount > 10 && p1.BlcCount > 10)
+                        break;
+                }
+                else
+                {
+                    blckCount = 0;
+                }
+
+                if (p0.Terminated && p1.Blocked)
+                    break;
+
+                if (p1.Terminated && p0.Blocked)
+                    break;
+
+                if (p0.Terminated && p1.Terminated)
+                    break;
+            }
+
+            Console.WriteLine("Result: " + p1.sndCount);
         }
 
-
-
-        private static long Common(string input, int offset) =>
-            input.Where((cur, i) => cur == input[(i + offset) % input.Length])
-                 .Sum(t => (long) char.GetNumericValue(t));
 
         class Prog
         {
             private readonly List<string> commands;
             private readonly int? id;
             private readonly Dictionary<string, long> reg;
+            private readonly Queue<long> myQueue;
+            private readonly Queue<long> otherQueue;
             private long lastFreq;
             public long? result = null;
             private int i = 0;
+            public int sndCount = 0;
 
-            public Prog(List<string> commands, int? id)
+            public Prog(List<string> commands, int? id, Queue<long> myQueue = null, Queue<long> otherQueue = null)
             {
                 this.commands = commands;
                 this.id = id;
+                this.myQueue = myQueue;
+                this.otherQueue = otherQueue;
                 this.reg = new Dictionary<string, long>();
                 if (id > 0)
                     reg["p"] = id.Value;
             }
 
+            public bool Blocked { get; set; }
+
+            public int BlcCount { get; set; }
+
+            public bool Terminated { get; set; }
+
             public void Run()
             {
-                while (i >= 0 && i < commands.Count)
+                if (Terminated)
+                    return;
+
+                while (true)
                 {
+                    if (i < 0 && i >= commands.Count)
+                    {
+                        Terminated = true;
+                        break;
+                    }
+
                     i += Process(commands[i]);
                     if (id == null && result != null)
+                        break;
+
+                    if (Blocked)
                         break;
                 }
             }
@@ -61,7 +114,12 @@ namespace AdventOfCode2017.Days
             {
                 if (command.StartsWith("snd"))
                 {
-                    lastFreq = GetValue(command.Substring(4));
+                    var val = GetValue(command.Substring(4));
+                    sndCount++;
+                    if (otherQueue != null)
+                        otherQueue.Enqueue(val);
+                    else
+                        lastFreq = val;
                 }
                 else if (command.StartsWith("set"))
                 {
@@ -85,8 +143,20 @@ namespace AdventOfCode2017.Days
                 }
                 else if (command.StartsWith("rcv"))
                 {
-                    var arg = GetValue(command.Substring(4));
-                    if (arg != 0)
+                    var arg = command.Substring(4);
+                    if (myQueue != null)
+                    {
+                        Blocked = false;
+                        if (!myQueue.TryDequeue(out var val))
+                        {
+                            Blocked = true;
+                            BlcCount++;
+                            return 0;
+                        }
+                        BlcCount = 0;
+                        reg[arg] = val;
+                    }
+                    else if (GetValue(arg) != 0)
                         result = lastFreq;
                 }
                 else if (command.StartsWith("jgz"))
