567c4606733e54ac589a9174d2e87c6c40a69545
Jani Bevk <jani.bevk@hotmail.com>
2019-12-01 09:03:02 +0100

    day 1

--- src/advent-of-code-2019/Days/Day01.cs
+++ src/advent-of-code-2019/Days/Day01.cs
@@ -1,100 +1,73 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using AdventOfCode.Common;
 using Xunit;
 
 namespace AdventOfCode.Y2018.Days
 {
     /*
---- Day 1: Chronal Calibration ---
+--- Day 1: The Tyranny of the Rocket Equation ---
 
-"We've detected some temporal anomalies,"
-one of Santa's Elves at the Temporal Anomaly Research and Detection Instrument Station tells you. 
-She sounded pretty worried when she called you down here.
-"At 500-year intervals into the past, someone has been changing Santa's history!"
+Santa has become stranded at the edge of the Solar System while delivering presents to other planets!
+To accurately calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas,
+he needs you to bring him measurements from fifty stars.
 
-"The good news is that the changes won't propagate to our time stream for another 25 days,
-and we have a device" - she attaches something to your wrist - "that will let you fix the changes with no such propagation delay.
-It's configured to send you 500 years further into the past every few days; that was the best we could do on such short notice."
-
-"The bad news is that we are detecting roughly fifty anomalies throughout time; the device will indicate fixed anomalies with stars.
-The other bad news is that we only have one device and you're the best person for the job! Good lu--" She taps a button on the device
-and you suddenly feel like you're falling. To save Christmas, you need to get all fifty stars by December 25th.
-
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the advent calendar;
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar;
 the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
 
-After feeling like you've been falling for a few minutes, you look at the device's tiny screen.
-"Error: Device must be calibrated before first use. Frequency drift detected. Cannot maintain destination lock."
-Below the message, the device shows a sequence of changes in frequency (your puzzle input).
-A value like +6 means the current frequency increases by 6; a value like -3 means the current frequency decreases by 3.
+The Elves quickly load you into a spacecraft and prepare to launch.
 
-For example, if the device displays frequency changes of +1, -2, +3, +1,
-then starting from a frequency of zero, the following changes would occur:
+At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. They haven't determined the amount of fuel required yet.
 
-    Current frequency  0, change of +1; resulting frequency  1.
-    Current frequency  1, change of -2; resulting frequency -1.
-    Current frequency -1, change of +3; resulting frequency  2.
-    Current frequency  2, change of +1; resulting frequency  3.
+Fuel required to launch a given module is based on its mass. Specifically,
+to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
 
-In this example, the resulting frequency is 3.
+For example:
 
-Here are other example situations:
+    For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+    For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+    For a mass of 1969, the fuel required is 654.
+    For a mass of 100756, the fuel required is 33583.
 
-    +1, +1, +1 results in  3
-    +1, +1, -2 results in  0
-    -1, -2, -3 results in -6
+The Fuel Counter-Upper needs to know the total fuel requirement.
+To find it, individually calculate the fuel needed for the mass of each module (your puzzle input),
+then add together all the fuel values.
 
-Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
+What is the sum of the fuel requirements for all of the modules on your spacecraft?
 
 --- Part Two ---
 
-You notice that the device repeats the same frequency change list over and over.
-To calibrate the device, you need to find the first frequency it reaches twice.
+During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence.
+Apparently, you forgot to include additional fuel for the fuel you just added.
 
-For example, using the same list of changes above, the device would loop as follows:
+Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2.
+However, that fuel also requires fuel, and that fuel requires fuel, and so on.
+Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass,
+if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
 
-    Current frequency  0, change of +1; resulting frequency  1.
-    Current frequency  1, change of -2; resulting frequency -1.
-    Current frequency -1, change of +3; resulting frequency  2.
-    Current frequency  2, change of +1; resulting frequency  3.
-    (At this point, the device continues from the start of the list.)
-    Current frequency  3, change of +1; resulting frequency  4.
-    Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+So, for each module mass, calculate its fuel and add it to the total.
+Then, treat the fuel amount you just calculated as the input mass and repeat the process,
+continuing until a fuel requirement is zero or negative. For example:
 
-In this example, the first frequency reached twice is 2.
-Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found,
-and that duplicates might be found while in the middle of processing the list.
+    A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0,
+        which would call for a negative fuel), so the total fuel required is still just 2.
+    At first, a module of mass 1969 requires 654 fuel. 
+        Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel,
+        which requires 5 fuel, which requires no further fuel.
+        So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+    The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
 
-Here are other examples:
-
-    +1, -1 first reaches 0 twice.
-    +3, +3, +4, -2, -4 first reaches 10 twice.
-    -6, +3, +8, +5, -6 first reaches 5 twice.
-    +7, +7, -2, -7, -4 first reaches 14 twice.
-
-What is the first frequency your device reaches twice?
+What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking
+into account the mass of the added fuel? (Calculate the fuel requirements for each module separately,
+then add them all up at the end.)
      */
 
     public class Day01 : DayBase
     {
-        public override object Part1() => Parse(Input).Sum();
+        public override object Part1() => Parse(Input).Select(x => (x / 3) - 2).Sum();
 
-        public override object Part2()
-        {
-            var set = new HashSet<int>();
-            int sum = 0;
+        public override object Part2() => Parse(Input).Select(x => Fuel(x) - x).Sum();
 
-            foreach(var i in Parse(Input).RepeatForever())
-            {
-                if (set.Contains(sum += i))
-                    break;
-
-                set.Add(sum);
-            }
-
-            return sum;
-        }
+        private static int Fuel(int mass) => mass <= 0 ? 0 : (mass + Fuel((mass / 3) - 2));
 
         private static IEnumerable<int> Parse(string input) => input.Split("\n").Select(int.Parse);
 
@@ -102,8 +75,8 @@ What is the first frequency your device reaches twice?
         public static void Test()
         {
             var day = Program.CreateInstance(1);
-            Assert.Equal(540, day.Part1());
-            Assert.Equal(73056, day.Part2());
+            Assert.Equal(3336439, day.Part1());
+            Assert.Equal(5001791, day.Part2());
         }
     }
 }
---
+++ src/advent-of-code-2019/Days/Day02.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Y2018.Days
+{
+    /*
+
+     */
+
+    public class Day02 : DayBase
+    {
+        public override object Part1() => Parse(Input).Select(x => (x / 3) - 2).Sum();
+
+        public override object Part2() => Parse(Input).Select(x => Fuel(x) - x).Sum();
+
+        private static int Fuel(int mass) => mass <= 0 ? 0 : (mass + Fuel((mass / 3) - 2));
+
+        private static IEnumerable<int> Parse(string input) => input.Split("\n").Select(int.Parse);
+
+        [Fact]
+        public static void Test()
+        {
+            var day = Program.CreateInstance(2);
+            Assert.Equal(3336439, day.Part1());
+            Assert.Equal(5001791, day.Part2());
+        }
+    }
+}
