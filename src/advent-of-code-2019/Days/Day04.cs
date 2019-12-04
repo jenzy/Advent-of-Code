ce1369b2766ce25039882ebbe33b7bdbbfc3288c
jenzy <bevk.jani@gmail.com>
2019-12-04 06:36:02 +0100

    day 4

--- src/advent-of-code-2019/Days/Day04.cs
+++ src/advent-of-code-2019/Days/Day04.cs
@@ -1,36 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using AdventOfCode.Common;
 using Xunit;
 
 namespace AdventOfCode.Y2019.Days
 {
     /*
+--- Day 4: Secure Container ---
 
+You arrive at the Venus fuel depot only to discover it's protected by a password.
+The Elves had written the password on a sticky note, but someone threw it out.
+
+However, they do remember a few key facts about the password:
+
+    It is a six-digit number.
+    The value is within the range given in your puzzle input.
+    Two adjacent digits are the same (like 22 in 122345).
+    Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+
+Other than the range rule, the following are true:
+
+    111111 meets these criteria (double 11, never decreases).
+    223450 does not meet these criteria (decreasing pair of digits 50).
+    123789 does not meet these criteria (no double).
+
+How many different passwords within the range given in your puzzle input meet these criteria?
+
+--- Part Two ---
+
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+    112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+    123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+    111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+
+How many different passwords within the range given in your puzzle input meet all of the criteria?
      */
 
     public class Day04 : DayBase
     {
         public override object Part1()
         {
-            return -1;
+            var limits = Parse(Input);
+
+            int count = 0;
+            for (int n = limits[0]; n <= limits[1]; n++)
+            {
+                var str = n.ToString();
+                if (IsAsc(str) && str.Distinct().Count() < 6)
+                    count++;
+            }
+
+            return count;
         }
 
         public override object Part2()
         {
-            return -1;
+            var limits = Parse(Input);
+
+            int count = 0;
+            for (int n = limits[0]; n <= limits[1]; n++)
+            {
+                var str = n.ToString();
+                if (IsAsc(str))
+                {
+                    for (int j = 1; j <= 5; j++)
+                    {
+                        if (str[j-1] == str[j]
+                            && (j == 5 || str[j] != str[j+1])
+                            && (j == 1 || str[j] != str[j-2]))
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return count;
         }
 
-        private static List<string[]> Parse(string input) => input.Split("\n").Select(x => x.Split(",")).ToList();
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IsAsc(string str)
+        {
+            Debug.Assert(str.Length == 6);
+            return str[0] <= str[1] && str[1] <= str[2] && str[2] <= str[3] && str[3] <= str[4] && str[4] <= str[5];
+        }
+
+        private static List<int> Parse(string input) => input.Split("-").Select(int.Parse).ToList();
 
         [Fact]
         public static void Test()
         {
             var day = Program.CreateInstance(4);
-            Assert.Equal(3247, day.Part1());
-            Assert.Equal(48054, day.Part2());
+            Assert.Equal(1675, day.Part1());
+            Assert.Equal(1142, day.Part2());
         }
     }
 }
---
+++ src/advent-of-code-2019/Days/Day05.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Y2019.Days
+{
+    /*
+
+     */
+
+    public class Day05 : DayBase
+    {
+        public override object Part1()
+        {
+            var limits = Parse(Input);
+            int count = 0;
+            return count;
+        }
+
+        public override object Part2()
+        {
+            var limits = Parse(Input);
+            int count = 0;
+            return count;
+        }
+
+        private static List<int> Parse(string input) => input.Split("-").Select(int.Parse).ToList();
+
+        [Fact]
+        public static void Test()
+        {
+            var day = Program.CreateInstance(5);
+            Assert.Equal(1675, day.Part1());
+            Assert.Equal(1142, day.Part2());
+        }
+    }
+}
